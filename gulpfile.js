b31c0c704792a3cbffa0f8932516658869b603af
pangyongsheng <yongsheng_pang.@outlook.com>
2017-12-27 11:21:47 +0800

    readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,3 @@
-/**
- * Created by dongfenghua on 2017/2/27.
- */
-//使用严格模式的js。保证js的严谨，作为一个好习惯。
 'use strict';
 const gulp = require('gulp');
 /*global -$ 加载需使用的gulp插件*/
@@ -9,11 +5,9 @@ const $ = require('gulp-load-plugins')();
 
 /*
  *管理资源文件路径集合
- *config.src下
- *css scripts images 替换为自己的路径(可按照此配置管理)
  */
 var config = {};
-// 源资源文件路径
+
 config['src'] = { 
     scripts: 'src/*.js',
 };
